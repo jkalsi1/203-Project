1b48c40f8bd8768c84ccdeec4b302a46b62fe486
Junior <jpere262@calpoly.edu>
2022-11-21 14:46:28 -0800

    Working on wyvern class

--- src/Wyvern.java
+++ src/Wyvern.java
@@ -2,8 +2,26 @@ import processing.core.PImage;
 
 import java.util.List;
 
-public class Wyvern extends AnimatedEntity {
-    public Wyvern(String id, Point position, List<PImage> images, int animationPeriod, int health, int healthLimit) {
-        super(id, position, images, animationPeriod, health, healthLimit);
-    }
+public class Wyvern  {
+//    public Wyvern(String id, Point position, List<PImage> images, int animationPeriod, int health, int healthLimit) {
+//        super(id, position, images, animationPeriod, health, healthLimit);
+//    }
+//
+////    public static boolean parseWyvern(
+////            String[] properties, WorldModel world, ImageStore imageStore)
+////    {
+////        if (properties.length == WYVERN_NUM_PROPERTIES) {
+////            Point pt = new Point(Integer.parseInt(properties[WYVERN_COL]),
+////                    Integer.parseInt(properties[WYVERN_ROW]));
+////            OBSTACLE entity = new OBSTACLE(properties[WYVERN_ID], pt,
+////                    imageStore.getImageList(
+////                            WYVERN_KEY),
+////                    Integer.parseInt(properties[WYVERN_ANIMATION_PERIOD]),
+////                    0, 0
+////            );
+////            world.tryAddEntity(entity);
+////        }
+////
+////        return properties.length == WYVERN_NUM_PROPERTIES;
+////    }
 }
