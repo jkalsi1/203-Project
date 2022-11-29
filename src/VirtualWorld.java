c3949325aeaf482947e0916925bdc3e1a7db8879
Junior <jpere262@calpoly.edu>
2022-11-28 16:00:12 -0800

    Changed keyPressed() to now support player WASD controls (also moves the world camera with it).

--- src/VirtualWorld.java
+++ src/VirtualWorld.java
@@ -10,7 +10,7 @@ public final class VirtualWorld extends PApplet
 {
     private static final int TIMER_ACTION_PERIOD = 100;
 
-    private static final int VIEW_WIDTH = 640;
+    private static final int VIEW_WIDTH = 840;
     private static final int VIEW_HEIGHT = 480;
     private static final int TILE_WIDTH = 32;
     private static final int TILE_HEIGHT = 32;
@@ -40,9 +40,10 @@ public final class VirtualWorld extends PApplet
     private ImageStore imageStore;
     private WorldModel world;
     private WorldView view;
-
     private static EventScheduler scheduler;
+    private Player player;
     private long nextTime;
+    private int playerCheckCount;
 
     public static EventScheduler getScheduler() {
         return scheduler;
@@ -147,9 +148,56 @@ public final class VirtualWorld extends PApplet
         return view.getViewport().viewportToWorld(mouseX/TILE_WIDTH, mouseY/TILE_HEIGHT);
     }
     public void keyPressed() {
+        int dx = 0;
+        int dy = 0;
+        // FOR WASD MOVEMENT OF A SINGLE PLAYER ONLY
+        if (keyPressed)
+        {
+            // create new player (maximum of 1 per world) when 0 is pressed
+            if (key == '0' && playerCheckCount == 0)
+            {
+                player = new Player("player", new Point(20, 1), imageStore.getImageList("player"), 4,5,0,0 );
+                world.addEntity(player);
+                player.scheduleActions(scheduler, world, imageStore);
+                playerCheckCount++;
+            }
+            // move up with w (only works when there is an existing player)
+           if(key == 'w' || key == 'W' && playerCheckCount == 1)
+           {
+               dy = -1;
+               view.shiftView(dx, dy);
+               Point newPos = player.nextPositionPlayer(world, 1);
+               world.moveEntity(player, newPos);
+
+           }
+            // move up with s (only works when there is an existing player)
+            if(key == 's' || key == 'S' && playerCheckCount == 1)
+            {
+                dy = 1;
+                view.shiftView(dx, dy);
+                Point newPos = player.nextPositionPlayer(world, 2);
+                world.moveEntity(player, newPos);
+            }
+            // move up with w (only works when there is an existing player)
+            if(key == 'a' || key == 'A' && playerCheckCount == 1)
+            {
+                dx = -1;
+                view.shiftView(dx, dy);
+                Point newPos = player.nextPositionPlayer(world, 3);
+                world.moveEntity(player, newPos);
+            }
+            // move up with w (only works when there is an existing player)
+            if(key == 'd' || key == 'D' && playerCheckCount == 1)
+            {
+                dx = 1;
+                view.shiftView(dx, dy);
+                Point newPos = player.nextPositionPlayer(world, 4);
+                world.moveEntity(player, newPos);
+            }
+
+        }
         if (key == CODED) {
-            int dx = 0;
-            int dy = 0;
+
 
             switch (keyCode) {
                 case UP:
@@ -165,6 +213,7 @@ public final class VirtualWorld extends PApplet
                     dx = 1;
                     break;
             }
+
             view.shiftView(dx, dy);
         }
     }
@@ -202,6 +251,7 @@ public final class VirtualWorld extends PApplet
         try {
             Scanner in = new Scanner(new File(filename));
             world.load(in, imageStore);
+            // text for instructions on world
         }
         catch (FileNotFoundException e) {
             System.err.println(e.getMessage());
